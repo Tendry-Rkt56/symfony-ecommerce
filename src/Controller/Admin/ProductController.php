dc0b4c313514e55bc25a15b2f9fc2d21c7c70a21
Tendry <tendryzephyrin@gmail.com>
2024-11-27 20:55:30 +0100

    Mise en place du controller lié aux produits

---
+++ src/Controller/Admin/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/products', name: 'admin.products.')]
+class ProductController extends AbstractController
+{
+
+    public function __construct(private EntityManagerInterface $entity)
+    {
+        
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        $products = $this->entity->getRepository(Product::class)->findAll();
+        return $this->render('admin/product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+}

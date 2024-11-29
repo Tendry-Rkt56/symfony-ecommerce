34b5afacb602fb45df9a463275838cdcdc149079
Tendry <tendryzephyrin@gmail.com>
2024-11-29 19:02:18 +0100

    Ajout des contraintes de validation sur l'entité Product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,8 +4,11 @@ namespace App\Entity;
 
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[UniqueEntity('name', message:"Ce nom de produit existe déjà")]
 class Product
 {
     #[ORM\Id]
@@ -14,12 +17,14 @@ class Product
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(min:2, max:255)]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
     private ?string $slug = null;
 
     #[ORM\Column]
+    #[Assert\Positive()]
     private ?float $price = null;
 
     #[ORM\Column(length: 255, nullable: true)]

1c79c243c6ddeaf87d2805decd3a16d01e36c1a8
Tendry <tendryzephyrin@gmail.com>
2024-12-04 18:25:33 +0100

    Mise en place de l'administration des commandes

--- src/Controller/Admin/CommandeController.php
+++ src/Controller/Admin/CommandeController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Commande;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/admin/commande', name: 'admin.commande.')]
 class CommandeController extends AbstractController
 {
-    #[Route('/admin/commande', name: 'app_admin_commande')]
+
+    public function __construct(private EntityManagerInterface $entity)
+    {
+        
+    }
+
+    #[Route('/', name: 'index', methods:['GET'])]
     public function index(): Response
     {
+        $commandes = $this->entity->getRepository(Commande::class)->findAll();
         return $this->render('admin/commande/index.html.twig', [
-            
+            'commandes' => $commandes,
         ]);
     }
 }
